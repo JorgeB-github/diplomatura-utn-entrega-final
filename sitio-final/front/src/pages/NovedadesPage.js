a005d16c039a4e97dca01389469868ae0a8ad79d
jorge betas <devops.sky@nespon.com>
2024-05-05 01:41:07 -0300

    Conf para exponer lo de Node en React

--- sitio-final/front/src/pages/NovedadesPage.js
+++ sitio-final/front/src/pages/NovedadesPage.js
@@ -1,45 +1,32 @@
-import React from "react";
+import { useState, useEffect } from 'react';  
+import axios from 'axios';  
+import NovedadItem from '../components/novedades/NovedadItems';
 
-const FormPage = (props) => {
-    return (
-        <form className="row g-3">
-        <div className="col-md-6">
-          <label htmlFor="inputnombre" className="form-label">Nombre</label>
-          <input type="text" className="form-control" id="inputnombre" />
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="inputapellido" className="form-label">Apellido</label>
-          <input type="text" className="form-control" id="inputapellido" />
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="inputEmail4" className="form-label">Email</label>
-          <input type="email" className="form-control" id="inputEmail4" />
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="inputPassword4" className="form-label">Password</label>
-          <input type="password" className="form-control" id="inputPassword4" />
-        </div>
-        <div className="col-12">
-          <label htmlFor="inputdireccion" className="form-label">Direccion</label>
-          <input type="text" className="form-control" id="inputdireccion" />
-        </div>     
-        <div className="col-md-6">
-          <label htmlFor="inputciudad" className="form-label">Ciudad</label>
-          <input type="text" className="form-control" id="inputciudad" />
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="inputpais" className="form-label">País</label>
-          <select id="inputpais" className="form-select">
-            <option selected>Seleccione una opcion...</option>
-            <option>Argentina</option>
-            <option>Chile</option>
-          </select>
-        </div>        
-        <div className="col-12">
-          <button type="submit" className="btn btn-primary">Registrarse</button>
-        </div>
-      </form>
-    );
-}
-
-export default FormPage;
+const NovedadesPage = (props) => {  
+    const [loading, setLoading] = useState(false);  
+    const [novedades, setNovedades] = useState([]);  
+    
+    useEffect(() => {  
+        const cargarNovedades = async () => {  
+            setLoading(true);                          
+            const response = await axios.get('http://localhost:3000/api/novedades');  
+            setNovedades(response.data);  
+            setLoading(false);  
+        };  
+        
+        cargarNovedades();  
+        }, []);
+    
+        return (
+            <section className="holder">
+                <h2>Novedades</h2>
+                {loading ? (
+                    <p>Cargando...</p>
+                ) : (
+                    novedades.map(item => <NovedadItem key={item.id} title={item.titulo} subtitle={item.subtitulo} imagen={item.imagen} body={item.cuerpo} />)
+                )}
+            </section>
+        );
+    };
+    
+    export default NovedadesPage;
