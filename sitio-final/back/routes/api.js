69a586eea9909bff3f4a626115178fbcf5f4c6bd
jorge betas <devops.sky@nespon.com>
2024-05-06 16:06:48 -0300

    Envio de mails

--- sitio-final/back/routes/api.js
+++ sitio-final/back/routes/api.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
 
+var nodemailer = require('nodemailer'); //definimos quien procesa o quien va hacer el encargado de procesar los datos
+
 router.get('/novedades', async function(req, res, next) {
     let novedades = await novedadesModel.getNovedades();
 
@@ -24,8 +26,37 @@ router.get('/novedades', async function(req, res, next) {
             }
         }
     });    
+
     res.json(novedades);
+
 });
+
+router.post('/form', async (req, res) => { 
+    const mail = {                                                                               // Creo a donde envio el mail
+        to: 'hector.betas@gmail.com',  
+        subject: 'Contacto web',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere más informacion a 
+        este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje} 
+        <br> Su tel es: ${req.body.telefono}`
+    }
+
+    const transport = nodemailer.createTransport({  
+        host: process.env.SMTP_HOST,  
+        port: process.env.SMTP_PORT,  
+        auth:  {
+            user: process.env.SMTP_USER,  
+            pass: process.env.SMTP_PASS  
+        }  
+    }); //cierre transp  
+
+    await transport.sendMail(mail)  
+
+    res.status(201).json ({  
+        error: false,  
+        message: "Mensaje enviado"  
+    });  
+});  // cierra post/api
+
 
 
 module.exports = router;
--- sitio-final/front/src/pages/FormPage.js
+++ sitio-final/front/src/pages/FormPage.js
@@ -1,44 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import axios from 'axios';
 
 const FormPage = (props) => {
+      const initialForm = {  
+        nombre: '',
+        email: '',  
+        telefono: '',
+        mensaje: '' 
+      }  
+      
+      const [sending, setSending] = useState(false);  
+      const [msg, setMsg] = useState('');
+      const [formData, setFormData] = useState(initialForm);  
+      
+      const handlechange = e => {  
+          const { name, value } = e.target;  
+          setFormData(oldData => ({  
+              ...oldData,  
+              [name]: value //forma dinamica  
+      }));  
+      }
+
+      
+      const handleSubmit= async e => {  
+       e.preventDefault();  
+       setMsg('');  
+       setSending(true)  
+       const response = await axios.post("http://localhost:3000/api/form", formData);  
+       setSending(false);  
+       setMsg(response.data.message);  
+       if (response.data.error === false) {  
+           setFormData(initialForm)  
+      }
+    }
+
+
     return (
-        <form className="row g-3">
-        <div className="col-md-6">
-          <label htmlFor="inputnombre" className="form-label">Nombre</label>
-          <input type="text" className="form-control" id="inputnombre" />
+      <main className="holder contacto">
+        <div>
+            <h3>Contacto</h3>
+              <form action="/form" method="post" onSubmit= {handleSubmit} className="row g-3">
+              <div className="col-md-6">
+                <label htmlFor="inputnombre" className="form-label">Nombre</label>
+                <input type="text" className="form-control" id="inputnombre" name="nombre" value={formData.nombre} onChange={handlechange} />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="inputapellido" className="form-label">Email</label>
+                <input type="text" className="form-control" id="inputapellido" name="email" value={formData.email} onChange={handlechange} />
+              </div>
+              <div className="col-md-6">
+                <label htmlFor="inputEmail4" className="form-label">Telefono</label>
+                <input type="text" className="form-control" id="inputEmail4" name="telefono" value={formData.telefono } onChange={handlechange} />
+              </div>            
+              <div className="col-md-6">
+                <label htmlFor="inputciudad" className="form-label">Mensaje</label>
+                <textarea className="form-control" id="inputciudad" name="mensaje" value={formData.mensaje} onChange={handlechange} />
+              </div>          
+              <div className="col-12">
+                <button type="submit" className="btn btn-primary" value="Enviar">Enviar</button>
+              </div>      
+            </form>
+
+            {sending ? <p>Enviando...</p> : null}
+            {msg ? <p>{msg}</p> : null}
         </div>
-        <div className="col-md-6">
-          <label htmlFor="inputapellido" className="form-label">Apellido</label>
-          <input type="text" className="form-control" id="inputapellido" />
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="inputEmail4" className="form-label">Email</label>
-          <input type="email" className="form-control" id="inputEmail4" />
-        </div>
-        <div className="col-md-6">
-          <label htmlFor="inputPassword4" className="form-label">Password</label>
-          <input type="password" className="form-control" id="inputPassword4" />
-        </div>
-        <div className="col-12">
-          <label htmlFor="inputdireccion" className="form-label">Direccion</label>
-          <input type="text" className="form-control" id="inputdireccion" />
-        </div>     
-        <div className="col-md-6">
-          <label htmlFor="inputciudad" className="form-label">Ciudad</label>
-          <input type="text" className="form-control" id="inputciudad" />
-        </div>
-        <div className="col-md-4">
-          <label htmlFor="inputpais" className="form-label">País</label>
-          <select id="inputpais" className="form-select">
-            <option selected>Seleccione una opcion...</option>
-            <option>Argentina</option>
-            <option>Chile</option>
-          </select>
-        </div>        
-        <div className="col-12">
-          <button type="submit" className="btn btn-primary">Registrarse</button>
-        </div>
-      </form>
+        </main>
     );
 }
 
